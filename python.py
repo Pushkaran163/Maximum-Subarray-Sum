e3ca2fc7c466996b99546e3fda2f96c4344c5905
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-11-16 16:04:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -15,14 +15,11 @@ def max_sum_subarray1(arr):
 
 def max_sum_subarray2(arr):
     n = len(arr)
-    max_sum = 0
-    curr_sum = 0
+    max_sum = arr[0]
+    curr_sum = arr[0]
     
-    for i in range(n):
+    for i in range(1, n):
         curr_sum = max(arr[i], curr_sum + arr[i])
         max_sum = max(max_sum, curr_sum)
     
     return max_sum
-if __name__ == "__main__":
-    arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-    print(max_sum_subarray2(arr)) # 6
